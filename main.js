727f34afdfcf18b9c81388dd2971f7d33ccfc738
IKLANLO <ilandaberea@yahoo.es>
2023-11-03 20:56:17 +0100

    version sin extras

---
+++ main.js
@@ -0,0 +1,39 @@
+const usersBtn = document.querySelector('#buttonUsers')
+const usersDOM = document.querySelector('#buttonDOM')
+const usersCont = document.querySelector('#usersContainer')
+
+axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((res) => {
+        const { data } = res
+        data.forEach((user) => {
+            console.log(user)
+        })
+    })
+    .catch((err) => console.error(err))
+
+axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((res) => {
+        const { data } = res
+        data.forEach((user) => {
+            console.log(user.name)
+        })
+    })
+    .catch((err) => console.error(err))
+
+const users = axios.get('https://jsonplaceholder.typicode.com/users')
+
+const usersFunction = () => console.log(users)
+
+const usersDOMFunction = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((res) => {
+        const { data } = res
+        data.forEach((user) => {
+            usersCont.innerHTML += `<p>${user.name}</p>`
+        })
+    })
+    .catch((err) => console.error(err))
+}
+
+usersBtn.addEventListener('click',usersFunction)
+usersDOM.addEventListener('click', usersDOMFunction)
